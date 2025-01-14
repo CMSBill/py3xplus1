d9f1ed6fafd4242badd64e32c771750ce65e60c1
CMSBill <bill@cmsnova.com>
2025-01-14 19:56:37 +0000

    thuipgfjksdfjios

--- thing.py
+++ thing.py
@@ -1,12 +1,21 @@
 thing = int(input("a "))
 i=0
-while thing != 0:
-    if thing % 2 == 0:
+
+def checkthing(thething):
+    if thething % 2 == 0:
         # Even 
-        thing = thing/2
+        thething = thething/2
     else:
         #odd
-        thing = (thing*3)+1
+        thething = (thething*3)+1
+    return(thething)
+
+x=3
+while x != 0:
+    thing=checkthing(thing)
     i+=1
     print(f"{thing} {i}")
+    if thing==1:
+        x-=1
+
 
